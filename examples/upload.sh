960e8590dc02ab689ef4690931d81897477d0d8c
Ben Parees <bparees@redhat.com>
2014-08-29 11:37:09 -0400

    rework upload script

--- examples/upload.sh
+++ examples/upload.sh
@@ -2,7 +2,8 @@
 
 # Shell script written by Aaron Stannard - designed to do a multi-part FORM post with an image and upload it to IFS
 
-FILE="myfile.txt"
+HOST=$1
+FILE=$2
 TYPE="image/jpeg"
 
-curl -F "token=secret" -F "filename=$FILE;$TYPE;" -F "name=$FILE" -F "image=@$FILE" http://fileserver-bparees.rhcloud.com/$FILE
+curl -k -F "token=secret" -F "filename=$FILE;$TYPE;" -F "name=$FILE" -F "image=@$FILE" https://$HOST/
